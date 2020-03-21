22fd8e6f6475112b5a4b73772dba1444e1a02883
Jesus Aranda <aranda.jm@icloud.com>
2020-03-21 10:47:22 -0300

    Creating App component

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+// Allows to render react component into the dom!
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: __dirname,
     filename: "bundle.js"
   },
+  devServer: {
+    historyApiFallback: true
+  },
   devtool: "eval-source-map",
   module: {
     loaders: [
@@ -18,6 +21,11 @@ module.exports = {
           presets: ["es2015", "react"],
           plugins: ["transform-object-rest-spread"]
         }
+      },
+      {
+        loader:
+          "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
+        test: /\.css$/
       }
     ]
   }
