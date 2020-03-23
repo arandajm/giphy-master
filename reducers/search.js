a64d1a600ddc2087c8a59163b093f52ad67126c3
Jesus Aranda <aranda.jm@icloud.com>
2020-03-23 13:39:39 -0300

    Getting The Search Result Images and transform it into the expected estructure

--- reducers/search.js
+++ reducers/search.js
@@ -4,6 +4,16 @@ const initialState = {
   results: []
 };
 
+function searchResultTransformer(element) {
+  const { images } = element;
+  return {
+    thumbnail: images.fixed_height_small_still
+      ? images.fixed_height_small_still.url
+      : null,
+    full: images.original ? images.url : null
+  };
+}
+
 export default (state, action) => {
   if (state === undefined) {
     return initialState;
@@ -12,7 +22,8 @@ export default (state, action) => {
     case SEARCH_SUCCESS:
       return {
         ...state,
-        results: action.results
+        // Transform the original action results to the expected (SearchResult)
+        results: action.results.map(element => searchResultTransformer)
       };
     default:
       return state;
