99a140ef4036b46285109cee42e65208ac5ac5e9
Jesus Aranda <aranda.jm@icloud.com>
2020-03-25 16:53:45 -0300

    Styling the search results component

--- components/search-results/SearchResults.js
+++ components/search-results/SearchResults.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as CustomTypes from "../../lib/custom-types";
 import SearchResult from "../search-result/SearchResult";
+import styles from "./search-results.css";
 
 export default function SearchResults({ results }) {
   const searchResults = results.map((result, i) => {
     return <SearchResult result={result} key={i} />;
   });
-  return <div>{searchResults}</div>;
+  return <div className={styles.container}>{searchResults}</div>;
 }
 
 SearchResults.propTypes = {
